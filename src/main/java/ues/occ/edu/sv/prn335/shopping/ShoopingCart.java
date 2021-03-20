0264162dd25f60ffa1f4e54b4db9724946dc6f08
avalos <christian@avalosPC.com>
2021-03-20 11:50:14 -0600

    Practica 2 TestUnidad

---
+++ src/main/java/ues/occ/edu/sv/prn335/shopping/ShoopingCart.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ues.occ.edu.sv.prn335.shopping;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ *
+ * @author christian
+ */
+public class ShoopingCart {
+    
+    private ArrayList items;
+    
+    public ShoopingCart() {
+        items = new ArrayList();
+    }
+    
+    public double getBalance() {
+        double balance = 0.00;
+        for (Iterator i = items.iterator(); i.hasNext();) {
+            Producto item = (Producto)i.next();
+            balance += item.getPrice();         
+        }
+        
+        return balance;
+    }
+    
+    public void addItem(Producto item) {
+        items.add(item);
+    }
+    
+    public void removeItem(Producto item) {
+        items.remove(item);
+    }
+    
+    public int getItemCount() {
+        return items.size();
+    }
+    
+    public void empty() {
+        items.clear();
+    }
+    
+}
